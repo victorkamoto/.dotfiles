e5e6ba36e06cf28727430211c1fc48eea44b808b
victorkamoto <victorkamoto@gmail.com>
2024-03-27 04:41:33 +0300

    nvim: relative numbers false!

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- winbar show current file path
 vim.opt.winbar = "%=%m %f"
+
+-- Turn off relative line numbers
+vim.wo.relativenumber = false
