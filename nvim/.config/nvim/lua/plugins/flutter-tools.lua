3e1b63173747daa936b4cf1f9db122ed60bd46b5
victorkamoto <victorkamoto@gmail.com>
2024-03-28 03:38:46 +0300

    nvim: flutter-tools

---
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = "true",
+    ft = "dart",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = true,
+  },
+}
